9aa1284db37c5dff444148a1601a000b6f93d2e4
Khanh Son Pham <phamson5kmmr@gmail.com>
2025-03-31 14:36:28 +0200

    added restart_nvidia driver

--- start_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Pulling Qdrant Docker image..."
-docker pull qdrant/qdrant
-
-echo "Starting Qdrant container..."
-docker run -p 6333:6333 -p 6334:6334 -v "$(pwd)/qdrant_storage:/qdrant/storage:z" qdrant/qdrant
-
-echo "Qdrant is running on ports 6333 (REST API) and 6334 (gRPC)."
